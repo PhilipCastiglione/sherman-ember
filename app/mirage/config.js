291b7f24fea302e8c5de2dce7b1170bcce896581
Phil DESTROYER OF WORLDS <phil@hhd.com.au>
2016-01-01 18:51:43 +0800

    added full board sample to mirage

--- app/mirage/config.js
+++ app/mirage/config.js
@@ -1,50 +1,62 @@
 export default function() {
   this.get('/tiles', function() {
     var tiles = [{
-      type: 'tiles',
-      id: 1,
-      attributes: {
-        piece: 'wall',
-        position: 1
-      }
-    },{
        type: 'tiles',
-      id: 2,
+      id: 50,
       attributes: {
         piece: 'floor',
-        position: 2
+        position: 50
       }
     },{
        type: 'tiles',
-      id: 3,
+      id: 51,
       attributes: {
         piece: 'subject',
-        position: 3
+        position: 51
       }
     },{
        type: 'tiles',
-      id: 4,
+      id: 52,
       attributes: {
         piece: 'player',
-        position: 4
+        position: 52
       }
     },{
        type: 'tiles',
-      id: 5,
+      id: 53,
       attributes: {
         piece: 'floor',
-        position: 5
+        position: 53
       }
     },{
       type: 'tiles',
-      id: 6,
+      id: 54,
       attributes: {
         piece: 'target',
-        position: 6
+        position: 54
       }
     }];
-    return {
-      data: tiles
+    for (var i = 1; i <= 49; i++) {
+      tiles.unshift({
+        type: 'tiles',
+        id: i,
+        attributes: {
+          piece: 'wall',
+          position: i
+        }
+      });
     }
+    for (var i = 55; i <= 16*8; i++) {
+      tiles.push({
+        type: 'tiles',
+        id: i,
+        attributes: {
+          piece: 'wall',
+          position: i
+        }
+      });
+    }
+    tiles.sort(function(a, b) { return a.id - b.id; });
+    return { data: tiles }
   });
 }
