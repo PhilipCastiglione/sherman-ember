ebb6243b15b0e73638b6a7938e69b2e6543396e9
Phil DESTROYER OF WORLDS <phil@hhd.com.au>
2016-01-01 17:43:38 +0800

    extracted tiles out in mirage generator in preparation for refactoring

--- app/mirage/config.js
+++ app/mirage/config.js
@@ -1,43 +1,50 @@
 export default function() {
   this.get('/tiles', function() {
+    var tiles = [{
+      type: 'tiles',
+      id: 1,
+      attributes: {
+        piece: 'wall',
+        position: 1
+      }
+    },{
+       type: 'tiles',
+      id: 2,
+      attributes: {
+        piece: 'floor',
+        position: 2
+      }
+    },{
+       type: 'tiles',
+      id: 3,
+      attributes: {
+        piece: 'subject',
+        position: 3
+      }
+    },{
+       type: 'tiles',
+      id: 4,
+      attributes: {
+        piece: 'player',
+        position: 4
+      }
+    },{
+       type: 'tiles',
+      id: 5,
+      attributes: {
+        piece: 'floor',
+        position: 5
+      }
+    },{
+      type: 'tiles',
+      id: 6,
+      attributes: {
+        piece: 'target',
+        position: 6
+      }
+    }];
     return {
-      data: [{
-        type: 'tiles',
-        id: 1,
-        attributes: {
-          piece: 'wall'
-        }
-      },{
-         type: 'tiles',
-        id: 2,
-        attributes: {
-          piece: 'floor'
-        }
-      },{
-         type: 'tiles',
-        id: 3,
-        attributes: {
-          piece: 'subject'
-        }
-      },{
-         type: 'tiles',
-        id: 4,
-        attributes: {
-          piece: 'player'
-        }
-      },{
-         type: 'tiles',
-        id: 5,
-        attributes: {
-          piece: 'floor'
-        }
-      },{
-        type: 'tiles',
-        id: 6,
-        attributes: {
-          piece: 'target'
-        }
-      }]
+      data: tiles
     }
   });
 }
