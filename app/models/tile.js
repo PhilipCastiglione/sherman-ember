497c84abe513ff44d5cebc57cb25d316b1c7ad79
Phil DESTROYER OF WORLDS <phil@hhd.com.au>
2016-01-01 18:52:09 +0800

    removed row and fixed col bug in tile

--- app/models/tile.js
+++ app/models/tile.js
@@ -5,10 +5,8 @@ export default DS.Model.extend({
   piece: DS.attr(),
   position: DS.attr(),
   col: Ember.computed('position', function() {
-    return this.get('position') % ENV.APP.game.columns;
-  }),
-  row: Ember.computed('position', 'row', function() {
-    return (this.get('position') - this.get('col')) / ENV.APP.game.columns + 1;
+    var c = this.get('position') % ENV.APP.game.columns;
+    return (c == 0)? ENV.APP.game.columns : c;
   }),
   actions: {
     move(direction) {
@@ -17,10 +15,10 @@ export default DS.Model.extend({
   init() {
     if (this.get('piece') == "player") {
       console.log('this is the player piece I guess');
-      console.log(this.get('col'));
-      console.log(this.get('row'));
       // attach event listeners for player piece to initiate moves
     }
+    console.log('col:');
+    console.log(this.get('col'));
     this._super(...arguments)
   }
 });
