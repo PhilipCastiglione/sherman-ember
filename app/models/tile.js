5301ae327a646ad929e16c6720123e74f112adae
Phil DESTROYER OF WORLDS <phil@hhd.com.au>
2016-01-02 07:56:42 +0800

    fixed minor errors in model and removed col computed property from tile

--- app/models/tile.js
+++ app/models/tile.js
@@ -4,21 +4,15 @@ import ENV from 'sherman-ember/config/environment';
 export default DS.Model.extend({
   piece: DS.attr(),
   position: DS.attr(),
-  col: Ember.computed('position', function() {
-    var c = this.get('position') % ENV.APP.game.columns;
-    return (c == 0)? ENV.APP.game.columns : c;
-  }),
   actions: {
     move(direction) {
     }
   },
   init() {
-    if (this.get('piece') == "player") {
+    if (this.get('piece') === "player") {
       console.log('this is the player piece I guess');
       // attach event listeners for player piece to initiate moves
     }
-    console.log('col:');
-    console.log(this.get('col'));
-    this._super(...arguments)
+    this._super(...arguments);
   }
 });
