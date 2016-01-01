795866f14f7355e23f2b3649e3f66f56df470199
Phil DESTROYER OF WORLDS <phil@hhd.com.au>
2016-01-01 17:44:07 +0800

    added position, and computed props col and row to tile model

--- app/models/tile.js
+++ app/models/tile.js
@@ -1,7 +1,15 @@
 import DS from 'ember-data';
+import ENV from 'sherman-ember/config/environment';
 
 export default DS.Model.extend({
   piece: DS.attr(),
+  position: DS.attr(),
+  col: Ember.computed('position', function() {
+    return this.get('position') % ENV.APP.game.columns;
+  }),
+  row: Ember.computed('position', 'row', function() {
+    return (this.get('position') - this.get('col')) / ENV.APP.game.columns + 1;
+  }),
   actions: {
     move(direction) {
     }
@@ -9,6 +17,8 @@ export default DS.Model.extend({
   init() {
     if (this.get('piece') == "player") {
       console.log('this is the player piece I guess');
+      console.log(this.get('col'));
+      console.log(this.get('row'));
       // attach event listeners for player piece to initiate moves
     }
     this._super(...arguments)
