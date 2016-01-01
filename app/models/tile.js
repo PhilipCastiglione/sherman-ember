35c6b749694c0b7ba6993da08967b11c74e35c05
Phil DESTROYER OF WORLDS <phil@hhd.com.au>
2016-01-01 15:15:45 +0800

    partial implementation of tile init for player

--- app/models/tile.js
+++ app/models/tile.js
@@ -1,5 +1,16 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  piece: DS.attr()
+  piece: DS.attr(),
+  actions: {
+    move(direction) {
+    }
+  },
+  init() {
+    if (this.get('piece') == "player") {
+      console.log('this is the player piece I guess');
+      // attach event listeners for player piece to initiate moves
+    }
+    this._super(...arguments)
+  }
 });
