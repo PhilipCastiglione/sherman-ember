67cf1d68c0f65b4cdae7dce42c33f1e411ab1daa
Phil DESTROYER OF WORLDS <phil@hhd.com.au>
2016-01-05 09:27:55 +1100

    added comment

--- app/models/tile.js
+++ app/models/tile.js
@@ -5,7 +5,14 @@ export default DS.Model.extend({
   piece: DS.attr(),
   position: DS.attr(),
   actions: {
-    move(direction) {
+    move(input) {
+      // convert the input to a direction
+      // using the current location of the piece and the direction, assign the target square
+      // check if the target square is valid:
+      //   - inside the game space
+      //   - not into a wall
+      // check if the target square is the subject
+      // if so, run move on the target square with the relevant input
     }
   },
   init() {
